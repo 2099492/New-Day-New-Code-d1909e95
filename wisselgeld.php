8a18cb1c9dee817b1281365803a7a9605335564b
2099492 <54841481+2099492@users.noreply.github.com>
2019-12-05 14:50:27 +0100

    Add files via upload

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,27 +1,27 @@
 <?php
-// $input = intval($argv[1]);
-// $data = array(10,5,2,1);
-// foreach ($data as $value) {
-//   $wissel = floor($input / $value);
-//   if ($wissel >= 1) {
-//     $input = $input - ($value * $wissel);
-//     echo $wissel . " x " . $value . PHP_EOL;
-//   }
-// }
+$input = intval($argv[1]);
+$cent = round($input, 2);
+$cent = $cent - 1 % $cent;
 
-echo " Hoeveel landen ga je toevoegen? ";
-$a = readline();
-$topo = array();
-if (!is_numeric($a)) {
-exit ($a . " is geen geldige getal, probeer het opnieuw ");
-}
-for ($i=0; $i < $a ; $i++) {
-    echo "Welk land wil je toevoegen?";
-    $topo[$b = readline()] = $c = readline(" Wat is de hoofdstad van " . $b . "?");
-}
-echo " De volgende landen en steden zitten in de database " . PHP_EOL;
-foreach ($topo as $key => $value) {
-  echo $key . "," . $value . PHP_EOL;
+$data = array(50,20,10,5,2,1);
+$data2 = array(50,20,10,5);
+
+foreach ($data as $value) {
+
+  $wissel = floor($input / $value);
+
+foreach ($data2 as $value) {
+  // code...
 }
 
- ?>
+  if ($wissel >= 1) {
+
+    $input = $input - ($value * $wissel);
+
+
+    echo $wissel . " x " . $value . " euro" . PHP_EOL;
+  }
+
+}
+
+?>
